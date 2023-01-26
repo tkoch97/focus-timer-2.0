de1755569a8eaeb727f5e43b18e30abe629bf204
Thiago Koch <tkoch.gim@gmail.com>
2023-01-26 20:49:42 -0300

    feat/fix: add new function in buttons and fix bugs

--- js/index.js
+++ js/index.js
@@ -1,92 +1,120 @@
+import Sounds from "./sounds.js";
+
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
-const buttonPlay = document.querySelector(".play");
-const buttonPause = document.querySelector(".pause");
-const buttonStop = document.querySelector(".stop");
-const buttonSumFiveMinutes = document.querySelector(".sumFiveMinutes");
-const buttonSubFiveMinutes = document.querySelector(".subFiveMinutes");
-
-const iconsButtonsSongs = {
-  forest: document.querySelector(".songButtons .forest svg path"),
-  rain: document.querySelector(".songButtons .rain svg path"),
-  coffeeShop: document.querySelector(".songButtons .coffeeShop svg path"),
-  fireplace: document.querySelector(".songButtons .fireplace svg path"),
+const sounds = Sounds ();
+const controls = {
+  play: document.querySelector(".play"),
+  pause: document.querySelector(".pause"),
+  stop: document.querySelector(".stop"),
+  SumFiveMinutes: document.querySelector(".sumFiveMinutes"),
+  SubFiveMinutes: document.querySelector(".subFiveMinutes"),
 }
 
-const buttonsSongs = {
+const buttonsSounds = {
   forest: document.querySelector(".forest"),
   rain: document.querySelector(".rain"),
   coffeeShop: document.querySelector(".coffeeShop"),
   fireplace: document.querySelector(".fireplace"),
 }
 
-buttonPlay.addEventListener('click', function () {
-  buttonPause.classList.remove('hide');
-  buttonPlay.classList.add('hide');
+const iconsButtonsSounds = {
+  forest: document.querySelector(".songButtons .forest svg path"),
+  rain: document.querySelector(".songButtons .rain svg path"),
+  coffeeShop: document.querySelector(".songButtons .coffeeShop svg path"),
+  fireplace: document.querySelector(".songButtons .fireplace svg path"),
+}
+
+controls.play.addEventListener('click', function () {
+  controls.pause.classList.remove('hide');
+  controls.play.classList.add('hide');
+  sounds.pressButtonSound();
 });
 
-buttonPause.addEventListener('click', function () {
-  buttonPause.classList.add('hide');
-  buttonPlay.classList.remove('hide');
+controls.pause.addEventListener('click', function () {
+  controls.pause.classList.add('hide');
+  controls.play.classList.remove('hide');
+  sounds.pressButtonSound();
 });
 
-buttonStop.addEventListener('click', function () {
-  buttonPause.classList.add('hide');
-  buttonPlay.classList.remove('hide');
+controls.stop.addEventListener('click', function () {
+  controls.pause.classList.add('hide');
+  controls.play.classList.remove('hide');
+  sounds.pressButtonSound();
 })
 
-buttonsSongs.forest.addEventListener('click', function(){
-  if (!buttonsSongs.forest.classList.contains("forestClicked")) {
-    buttonsSongs.forest.classList.add('forestClicked');
-    buttonsSongs.rain.classList.remove('rainClicked');
-    buttonsSongs.coffeeShop.classList.remove('coffeeShopClicked');
-    buttonsSongs.fireplace.classList.remove('fireplaceClicked');
-    iconsButtonsSongs.forest.setAttribute("fill", "#fff");
-    ["rain", "coffeeShop", "fireplace"].forEach(attribute => iconsButtonsSongs[attribute].setAttribute("fill", "#323238"));
+buttonsSounds.forest.addEventListener('click', function(){
+  if (!buttonsSounds.forest.classList.contains("forestClicked")) {
+    buttonsSounds.forest.classList.add('forestClicked');
+    buttonsSounds.rain.classList.remove('rainClicked');
+    buttonsSounds.coffeeShop.classList.remove('coffeeShopClicked');
+    buttonsSounds.fireplace.classList.remove('fireplaceClicked');
+    iconsButtonsSounds.forest.setAttribute("fill", "#fff");
+    ["rain", "coffeeShop", "fireplace"].forEach(attribute => iconsButtonsSounds[attribute].setAttribute("fill", "#323238"));
+    sounds.playForestSound();
+    sounds.stopFireplaceSound();
+    sounds.stopCoffeeShopSound();
+    sounds.stopRainSound();
   } else {
-    buttonsSongs.forest.classList.remove('forestClicked');
-    iconsButtonsSongs.forest.setAttribute("fill", "#323238");
+    buttonsSounds.forest.classList.remove('forestClicked');
+    iconsButtonsSounds.forest.setAttribute("fill", "#323238");
+    sounds.stopForestSound();
   }
 })
 
-buttonsSongs.rain.addEventListener('click', function(){
-  if (!buttonsSongs.rain.classList.contains("rainClicked")) {
-    buttonsSongs.rain.classList.add('rainClicked');
-    buttonsSongs.forest.classList.remove('forestClicked');
-    buttonsSongs.coffeeShop.classList.remove('coffeeShopClicked');
-    buttonsSongs.fireplace.classList.remove('fireplaceClicked');
-    iconsButtonsSongs.rain.setAttribute("fill", "#fff");
-    ["forest", "coffeeShop", "fireplace"].forEach(attribute => iconsButtonsSongs[attribute].setAttribute("fill", "#323238"));
+buttonsSounds.rain.addEventListener('click', function(){
+  if (!buttonsSounds.rain.classList.contains("rainClicked")) {
+    buttonsSounds.rain.classList.add('rainClicked');
+    buttonsSounds.forest.classList.remove('forestClicked');
+    buttonsSounds.coffeeShop.classList.remove('coffeeShopClicked');
+    buttonsSounds.fireplace.classList.remove('fireplaceClicked');
+    iconsButtonsSounds.rain.setAttribute("fill", "#fff");
+    ["forest", "coffeeShop", "fireplace"].forEach(attribute => iconsButtonsSounds[attribute].setAttribute("fill", "#323238"));
+    sounds.playRainSound();
+    sounds.stopForestSound();
+    sounds.stopFireplaceSound();
+    sounds.stopCoffeeShopSound();
   } else {
-    buttonsSongs.rain.classList.remove('rainClicked');
-    iconsButtonsSongs.rain.setAttribute("fill", "#323238");
+    buttonsSounds.rain.classList.remove('rainClicked');
+    iconsButtonsSounds.rain.setAttribute("fill", "#323238");
+    sounds.stopRainSound();
   }
 })
 
-buttonsSongs.coffeeShop.addEventListener('click', function(){
-  if (!buttonsSongs.coffeeShop.classList.contains("coffeeShopClicked")) {
-    buttonsSongs.coffeeShop.classList.add('coffeeShopClicked');
-    buttonsSongs.rain.classList.remove('rainClicked');
-    buttonsSongs.forest.classList.remove('forestClicked');
-    buttonsSongs.fireplace.classList.remove('fireplaceClicked');
-    iconsButtonsSongs.coffeeShop.setAttribute("fill", "#fff");
-    ["forest", "rain", "fireplace"].forEach(attribute => iconsButtonsSongs[attribute].setAttribute("fill", "#323238"));
+buttonsSounds.coffeeShop.addEventListener('click', function(){
+  if (!buttonsSounds.coffeeShop.classList.contains("coffeeShopClicked")) {
+    buttonsSounds.coffeeShop.classList.add('coffeeShopClicked');
+    buttonsSounds.rain.classList.remove('rainClicked');
+    buttonsSounds.forest.classList.remove('forestClicked');
+    buttonsSounds.fireplace.classList.remove('fireplaceClicked');
+    iconsButtonsSounds.coffeeShop.setAttribute("fill", "#fff");
+    ["forest", "rain", "fireplace"].forEach(attribute => iconsButtonsSounds[attribute].setAttribute("fill", "#323238"));
+    sounds.stopRainSound();
+    sounds.stopForestSound();
+    sounds.stopFireplaceSound();
+    sounds.playCoffeeShopSound();
   } else {
-    buttonsSongs.coffeeShop.classList.remove('coffeeShopClicked');
-    iconsButtonsSongs.coffeeShop.setAttribute("fill", "#323238");
+    buttonsSounds.coffeeShop.classList.remove('coffeeShopClicked');
+    iconsButtonsSounds.coffeeShop.setAttribute("fill", "#323238");
+    sounds.stopCoffeeShopSound();
   }
 })
 
-buttonsSongs.fireplace.addEventListener('click', function(){
-  if (!buttonsSongs.fireplace.classList.contains("fireplaceClicked")) {
-    buttonsSongs.fireplace.classList.add('fireplaceClicked');
-    buttonsSongs.rain.classList.remove('rainClicked');
-    buttonsSongs.forest.classList.remove('forestClicked');
-    buttonsSongs.coffeeShop.classList.remove('coffeeShopClicked');
-    iconsButtonsSongs.fireplace.setAttribute("fill", "#fff");
-    ["forest", "rain", "coffeeShop"].forEach(attribute => iconsButtonsSongs[attribute].setAttribute("fill", "#323238"));
+buttonsSounds.fireplace.addEventListener('click', function(){
+  if (!buttonsSounds.fireplace.classList.contains("fireplaceClicked")) {
+    buttonsSounds.fireplace.classList.add('fireplaceClicked');
+    buttonsSounds.rain.classList.remove('rainClicked');
+    buttonsSounds.forest.classList.remove('forestClicked');
+    buttonsSounds.coffeeShop.classList.remove('coffeeShopClicked');
+    iconsButtonsSounds.fireplace.setAttribute("fill", "#fff");
+    ["forest", "rain", "coffeeShop"].forEach(attribute => iconsButtonsSounds[attribute].setAttribute("fill", "#323238"));
+    sounds.stopRainSound();
+    sounds.stopForestSound();
+    sounds.stopCoffeeShopSound();
+    sounds.playFireplaceSound();
   } else {
-    buttonsSongs.fireplace.classList.remove('fireplaceClicked');
-    iconsButtonsSongs.fireplace.setAttribute("fill", "#323238");
+    buttonsSounds.fireplace.classList.remove('fireplaceClicked');
+    iconsButtonsSounds.fireplace.setAttribute("fill", "#323238");
+    sounds.stopFireplaceSound();
   }
 })
